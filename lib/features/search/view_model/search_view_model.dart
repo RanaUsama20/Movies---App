fb8abd24c020226615564b08665ddfb388a88ebd
ahmedgalal <ah.ga1599.aa@gmail.com>
2023-10-30 17:14:44 +0200

    finsh search page Ui

---
+++ lib/features/search/view_model/search_view_model.dart
@@ -0,0 +1,8 @@
+class MovieModel{
+  String? movieTitel;
+  String? moviePosterUrl;
+  double? rating;
+  int?  movieYear;
+
+  MovieModel(this.movieTitel, this.moviePosterUrl,this.rating,this.movieYear);
+}
